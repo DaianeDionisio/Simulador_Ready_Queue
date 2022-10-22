8359a63d362b9eb1f5c242cafdb9509461e3469b
DaianeDionisio <daianedionisio2000@gmail.com>
2022-10-22 12:07:54 -0300

    modificaçõez

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from Processo import Processo
+from Algoritmo import Algoritmo
 
 processos: list[Processo] = []
 
@@ -20,15 +21,20 @@ while (menu):
         priority = input()
         newProcess = Processo(process,arrivalTime,burstTime,priority)
         processos.append(newProcess)
+
         print("Deseja adicionar mais processos?:")
         print("1 - sim")
         print("2 - não")
-        resposta = input()
+        decisionProcess = input()
 
-        if(resposta == "2"):
+        if(decisionProcess == "2"):
             processMenu = False
 
     print("Escolha o algoritmo desejado:")
     print("1 - Priority Scheduling")
     print("2 - Round Robin")
+    decisioAlgorithm = input()
+    newAlgorithm = Algoritmo(decisioAlgorithm, processos)
+
+
 
